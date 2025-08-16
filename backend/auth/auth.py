0b2bbfaeb56694da34f77238f04b93434b6da160
adish-ct <adishathu525@gmail.com>
2025-08-16 17:03:17 +0530

    Feat(Register) : user registration logic implemented based on the use inputs

--- backend/auth/auth.py
+++ backend/auth/auth.py
@@ -1,6 +1,25 @@
 from sqlalchemy.orm import Session
+
+from core.security import hash_password
 from database.models.user import User
-from schemas.user import UserCreate, UserResponse
+from schemas.user import UserCreate
 
 def create_user(db: Session, user: UserCreate):
-    pass
+    # check if email already exists
+    existing_user = db.query(User).filter(User.email == user.email).first()
+    if existing_user:
+        raise ValueError("Email already registered.")
+
+    # Creating instance for User model based on the input.
+    db_user = User(
+        username=user.username,
+        email=user.email,
+        phone=user.phone,
+        hashed_password=hash_password(user.password)
+    )
+
+    db.add(db_user) # adding user to db
+    db.commit() # commiting the changes
+    db.refresh(db_user) # after commiting refreshing the db result.
+    return db_user
+
