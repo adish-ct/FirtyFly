98d9b70f14da552f9f59e3a459fa17655c360a3c
adish-ct <adishathu525@gmail.com>
2025-08-16 12:42:54 +0530

    Config(Database): created database and configured set up

--- backend/database.py
+++ backend/database.py
@@ -2,9 +2,25 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+# Update this when moving to production (don’t hardcode creds in
+# database server / username: password @ host / database name.
 DATABASE_URL = "postgresql://postgres:postgres@localhost/flirtfly"
+#for production : postgresql://username:password@db-host:5432/dbname
 
+
+# Create SQLAlchemy engine
 engine = create_engine(DATABASE_URL)
+
+# SessionLocal is used for DB sessions in routes
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+# Base class for ORM models
 base = declarative_base()
+
+# Dependency for FastAPI routes
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
