27067d01bf1c3c01911181556a3994e2207139ca
adish-ct <adishathu525@gmail.com>
2025-08-18 23:20:54 +0530

    Fix(excepiton): create user logic handled multiple exception

--- backend/routers/user.py
+++ backend/routers/user.py
@@ -1,24 +1,36 @@
 from fastapi import HTTPException, APIRouter, status, Depends
 from sqlalchemy.orm import Session
 
-from database.models.user import User
-from migrations.versions.ecfacd72486e_profile_table_created import depends_on
-from schemas.user import UserCreate, UserResponse
-from database.database import get_db
 from auth.auth import create_user
+from database.database import get_db
+from schemas.user import UserCreate, UserResponse
+from core.logger import logger
+
+# -----------------------
+# Router
+# -----------------------
 
 router = APIRouter(
     prefix="/users",
     tags=["users"]
 )
 
-@router.post("/register", response_model=UserResponse)
+@router.post("/register", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
 def register_user(user: UserCreate, db: Session = Depends(get_db)):
     try:
         db_user = create_user(db, user)
         return db_user
-    except Exception as e:
+
+    except ValueError as e:
+        logger.warning(f"Value Error Raised : {str(e)}")
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail=str(e)
+        )
+
+    except Exception as e:
+        logger.error(f'Exception Raised : {str(e)}')
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Some error occurred, please contact administrator."
         )
