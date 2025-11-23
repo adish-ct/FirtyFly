e61505a9b8ae8446439cf9394d28b9cc862a90e0
adish-ct <adishathu525@gmail.com>
2025-11-23 22:52:57 +0530

    Feat(tailwind) configured

--- frontend/flirtyfront/app/layout.jsx
+++ frontend/flirtyfront/app/layout.jsx
@@ -1,9 +1,23 @@
 import "./global.css";
+import { Toaster } from 'react-hot-toast'
 
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <body>{children}</body>
+            <body>
+                <Toaster
+                    position="top-center"
+                    toastOptions={{
+                        success: {
+                            style: { background: "#4ade80", color: "#fff" }
+                        },
+                        error: {
+                            style: { background: "#f87171", color: "#fff" }
+                        }
+                    }}
+                />
+                {children}
+            </body>
         </html>
     )
 }
